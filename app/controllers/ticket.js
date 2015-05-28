4c47f5fc20e4df15a3710848b8c5eba501803f6f
Jon Grimes <jong@spiceworks.com>
2015-05-28 09:14:43 -0500

    Status button works

--- app/controllers/ticket.js
+++ app/controllers/ticket.js
@@ -7,9 +7,24 @@ export default Ember.Controller.extend({
     return "#" + this.get("model.id");
   }.property("model.prettyId"),
 
+  isClosed: function() {
+    return this.get("model.isClosed");
+  }.property("model.isClosed"),
+
   actions: {
     saveChanges: function() {
       this.get("model").save();
+    },
+
+    toggleStatus: function() {
+      var ticket = this.get("model"),
+          isClosed = this.get("model.isClosed");
+
+      if (isClosed) {
+        ticket.set("status","open");
+      } else {
+        ticket.set("status","closed");
+      }
     }
   }
 });
--- server/mocks/tickets.js
+++ server/mocks/tickets.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
         {
             "id": 1,
             "creator": "Marlon Stehr",
-            "avatar_url": "http://robohash.org/perspiciatisplaceatfugit.png?size=300x300",
+            "avatar_url": "http://robohash.org/ametnonvoluptatem.png?size=150x150",
             "description": "Tempore nesciunt consequatur iusto eos.",
             "summary": "Aut consequuntur sunt repellendus necessitatibus voluptatem.",
             "status" : "closed",
@@ -15,7 +15,7 @@ module.exports = function(app) {
         {
             "id": 2,
             "creator": "Ms. Kristopher Dickinson",
-            "avatar_url": "http://robohash.org/perspiciatisplaceatfugit.png?size=300x300",
+            "avatar_url": "http://robohash.org/exercitationemdelectusdeleniti.png?size=150x150",
             "description": "Sit quia repudiandae dolor quo rerum.",
             "summary": "Commodi incidunt ab magni sed ea nam maxime enim.",
             "created_at": "2015-05-20T14:18:33-05:00"
@@ -23,7 +23,7 @@ module.exports = function(app) {
         {
             "id": 3,
             "creator": "Chance Kirlin",
-            "avatar_url": "http://robohash.org/perspiciatisplaceatfugit.png?size=300x300",
+            "avatar_url": "http://robohash.org/eaquecupiditatequia.png?size=150x150",
             "description": "Facere nihil alias voluptas rerum quibusdam sed sapiente vero. Corrupti modi at expedita nostrum fuga.",
             "summary": "Dolorum et odit ut est sunt maxime.",
             "created_at": "2015-05-12:18:33-05:00"
@@ -31,7 +31,7 @@ module.exports = function(app) {
         {
             "id": 4,
             "creator": "Foster Lockman",
-            "avatar_url": "http://robohash.org/perspiciatisplaceatfugit.png?size=300x300",
+            "avatar_url": "http://robohash.org/saepedolorebeatae.png?size=150x150",
             "description": "Maxime est tenetur aut vel. Culpa rerum nulla eum est. Quibusdam praesentium a delectus nobis dolores nostrum debitis incidunt.",
             "summary": "Praesentium tempore doloribus ut tempora est sed voluptas numquam iusto.",
             "created_at": "2015-05-20T14:18:33-05:00"
@@ -39,7 +39,7 @@ module.exports = function(app) {
         {
             "id": 5,
             "creator": "Dr. Ruthe Kuvalis",
-            "avatar_url": "http://robohash.org/perspiciatisplaceatfugit.png?size=300x300",
+            "avatar_url": "http://robohash.org/quaerataperiamcupiditate.png?size=150x150",
             "description": "Non omnis quos vel corporis voluptas. Ut molestiae ut doloremque impedit iste. Et sint repudiandae id fugit.",
             "summary": "Omnis ipsum sed aliquam minima et et.",
             "status" : "closed",
@@ -48,7 +48,7 @@ module.exports = function(app) {
         {
             "id": 6,
             "creator": "Anna Prosacco Sr.",
-            "avatar_url": "http://robohash.org/perspiciatisplaceatfugit.png?size=300x300",
+            "avatar_url": "http://robohash.org/voluptatemrerumdelectus.png?size=150x150",
             "description": "Ex molestias expedita aperiam voluptate. Tempore voluptatem adipisci repudiandae qui nesciunt. Ut tempore similique sequi beatae sunt.",
             "summary": "Et ullam quo ut commodi eum.",
             "created_at": "2015-05-20T04:18:33-05:00"
@@ -56,7 +56,7 @@ module.exports = function(app) {
         {
             "id": 7,
             "creator": "Reilly Goyette",
-            "avatar_url": "http://robohash.org/perspiciatisplaceatfugit.png?size=300x300",
+            "avatar_url": "http://robohash.org/providentnostrumcorporis.png?size=150x150",
             "description": "Reprehenderit corporis eligendi sed necessitatibus.",
             "summary": "Soluta sit voluptas temporibus necessitatibus magni.",
             "status" : "closed",
@@ -65,7 +65,7 @@ module.exports = function(app) {
         {
             "id": 8,
             "creator": "Jalon Dickens",
-            "avatar_url": "http://robohash.org/perspiciatisplaceatfugit.png?size=300x300",
+            "avatar_url": "http://robohash.org/nequevoluptatemperspiciatis.png?size=150x150",
             "description": "Quos eos et tempore at. Et consequatur assumenda magnam fugiat voluptatibus sequi exercitationem. Non consequuntur deleniti et est est ipsam.",
             "summary": "Repudiandae sunt molestiae dolorum commodi ut.",
             "created_at": "2015-05-20T14:18:33-05:00"
@@ -73,7 +73,7 @@ module.exports = function(app) {
         {
             "id": 9,
             "creator": "Reilly Macejkovic",
-            "avatar_url": "http://robohash.org/perspiciatisplaceatfugit.png?size=300x300",
+            "avatar_url": "http://robohash.org/quisautut.png?size=150x150",
             "description": "Occaecati natus dolor quis aperiam officiis reprehenderit consequatur voluptatem. Sint esse assumenda ipsa ut quia consectetur.",
             "summary": "Alias ut dolores suscipit et beatae.",
             "created_at": "2015-05-20T14:18:33-05:00"
@@ -81,7 +81,7 @@ module.exports = function(app) {
         {
             "id": 10,
             "creator": "Barrett Kunze",
-            "avatar_url": "http://robohash.org/perspiciatisplaceatfugit.png?size=300x300",
+            "avatar_url": "http://robohash.org/doloradipiscifugiat.png?size=150x150",
             "description": "Quas qui dolor id id illo molestiae quis ut. Non labore voluptatem atque eum.",
             "summary": "Tempore quod quo ipsa itaque mollitia provident.",
             "created_at": "2015-05-09T14:18:33-05:00"
@@ -89,7 +89,7 @@ module.exports = function(app) {
         {
             "id": 11,
             "creator": "Johnathan Goyette",
-            "avatar_url": "http://robohash.org/perspiciatisplaceatfugit.png?size=300x300",
+            "avatar_url": "http://robohash.org/aenimreiciendis.png?size=150x150",
             "description": "Voluptatum exercitationem accusantium aut iste maiores omnis.",
             "summary": "Dolor est saepe quas sapiente ut incidunt.",
             "created_at": "2015-05-20T14:18:33-05:00"
@@ -97,7 +97,7 @@ module.exports = function(app) {
         {
             "id": 12,
             "creator": "Maxine Tremblay",
-            "avatar_url": "http://robohash.org/perspiciatisplaceatfugit.png?size=300x300",
+            "avatar_url": "http://robohash.org/etatquelaborum.png?size=150x150",
             "description": "Eos alias commodi accusamus ducimus quibusdam id maxime. Et sapiente est et fuga et voluptatem sit a. Ipsum placeat in ea repellat praesentium.",
             "summary": "Et aspernatur voluptas quasi autem aut.",
             "created_at": "2015-05-20T14:18:33-05:00"
@@ -105,7 +105,7 @@ module.exports = function(app) {
         {
             "id": 13,
             "creator": "Sally Beier",
-            "avatar_url": "http://robohash.org/perspiciatisplaceatfugit.png?size=300x300",
+            "avatar_url": "http://robohash.org/mollitiaconsecteturaut.png?size=150x150",
             "description": "Libero aliquid quod temporibus architecto minus quia.",
             "summary": "Voluptatibus non illo numquam molestias amet aut eaque voluptatem.",
             "created_at": "2015-05-20T14:18:33-05:00"
@@ -113,7 +113,7 @@ module.exports = function(app) {
         {
             "id": 14,
             "creator": "Lucy Ortiz",
-            "avatar_url": "http://robohash.org/perspiciatisplaceatfugit.png?size=300x300",
+            "avatar_url": "http://robohash.org/eligendiaet.png?size=150x150",
             "description": "Ea ut illo molestias iusto hic et cupiditate.",
             "summary": "Eius libero sunt omnis vero saepe reiciendis omnis rerum laudantium.",
             "status" : "closed",
@@ -122,7 +122,7 @@ module.exports = function(app) {
         {
             "id": 15,
             "creator": "Lenora Dicki",
-            "avatar_url": "http://robohash.org/perspiciatisplaceatfugit.png?size=300x300",
+            "avatar_url": "http://robohash.org/involuptatumqui.png?size=150x150",
             "description": "Debitis dicta natus ut ex. Saepe enim odit soluta nobis.",
             "summary": "Impedit neque doloremque perferendis voluptatem quas ut quasi.",
             "created_at": "2015-05-20T14:18:33-05:00"
@@ -130,7 +130,7 @@ module.exports = function(app) {
         {
             "id": 16,
             "creator": "Mr. Geraldine Olson",
-            "avatar_url": "http://robohash.org/perspiciatisplaceatfugit.png?size=300x300",
+            "avatar_url": "http://robohash.org/temporaidmollitia.png?size=150x150",
             "description": "Inventore quaerat qui id iure aspernatur id voluptatem.",
             "summary": "Dolorum facilis veniam aut rerum vel aperiam.",
             "created_at": "2015-05-20T14:18:33-05:00"
@@ -138,7 +138,7 @@ module.exports = function(app) {
         {
             "id": 17,
             "creator": "Marisa Effertz",
-            "avatar_url": "http://robohash.org/perspiciatisplaceatfugit.png?size=300x300",
+            "avatar_url": "http://robohash.org/quamvoluptatemcumque.png?size=150x150",
             "description": "Id dolore accusamus sit eaque voluptas ipsam odit. Accusantium voluptates omnis excepturi optio quisquam laborum sed ut.",
             "summary": "Porro repellat molestiae aut illum consectetur.",
             "created_at": "2015-05-20T14:18:33-05:00"
@@ -146,7 +146,7 @@ module.exports = function(app) {
         {
             "id": 18,
             "creator": "Enid Nitzsche",
-            "avatar_url": "http://robohash.org/perspiciatisplaceatfugit.png?size=300x300",
+            "avatar_url": "http://robohash.org/temporadelectussunt.png?size=150x150",
             "description": "Qui iure aut reprehenderit est culpa est vitae tempore.",
             "summary": "Ratione ipsum corporis harum totam et aperiam illo.",
             "status" : "closed",
@@ -155,7 +155,7 @@ module.exports = function(app) {
         {
             "id": 19,
             "creator": "Billie Kessler MD",
-            "avatar_url": "http://robohash.org/perspiciatisplaceatfugit.png?size=300x300",
+            "avatar_url": "http://robohash.org/verovoluptatummaiores.png?size=150x150",
             "description": "Possimus alias ut sed officia. Nam dolorum quod aspernatur minima et officiis voluptatem. Molestiae quasi labore quos quod.",
             "summary": "Quis natus quis soluta recusandae voluptatem ipsam rem.",
             "status" : "closed",
@@ -164,7 +164,7 @@ module.exports = function(app) {
         {
             "id": 20,
             "creator": "Sarah DuBuque",
-            "avatar_url": "http://robohash.org/perspiciatisplaceatfugit.png?size=300x300",
+            "avatar_url": "http://robohash.org/voluptatemeumsit.png?size=150x150",
             "description": "Et voluptas ratione animi hic illum ut nemo. Et assumenda sunt non consectetur quia saepe consequatur itaque. Et nemo consectetur ducimus odit sequi rem qui ratione.",
             "summary": "Aut rem error reprehenderit et laboriosam esse in ipsum pariatur.",
             "created_at": "2015-05-20T14:18:33-05:00"
